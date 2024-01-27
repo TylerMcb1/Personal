a593f74e21c3d6fa6394a5283d2f12620241690d
Tyler M <tylermcbride@uchicago.edu>
2024-01-27 13:54:57 -0600

    Backup

--- cashman-flask-project/cashman/index.py
+++ cashman-flask-project/cashman/index.py
@@ -1,14 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return "Hello, World!"
+incomes = [
+    {'description': 'salary', 'amount': 5000}
+]
 
-@app.route('/<name>')
-def print_name(name):
-    return "Hello, {}".format(name)
+@app.route('/incomes')
+def get_incomes():
+    return jsonify(incomes)
 
-if __name__ == '__main__':
-    app.run(port = 5000)
+@app.route('/incomes', methods=['POST'])
+def add_income():
+    incomes.append(request.get_json())
+    return '', 204
+
+#if __name__ == '__main__':
+#    app.run(port = 5000)
