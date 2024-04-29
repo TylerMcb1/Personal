99661bea2b25908307dd4377b4c042b47cf638fe
Tyler M <tylermcbride@uchicago.edu>
2024-04-29 17:59:51 -0500

    Added display functions

--- intermodal-db-project/src/db.py
+++ intermodal-db-project/src/db.py
@@ -2,12 +2,12 @@ import mysql.connector
 from mysql.connector import Error
 
 class Reservation:
-    def __init__(self, reservation_id, first_name, last_name, 
-                 start, destination, start_time, duration):
+    def __init__(self, reservation_id: str, first_name: str, last_name: str, 
+                 start: str, destination: str, start_time: str, duration: int):
         """
         Initialize a Reservation object.
 
-        Args:
+        Parameters:
             res_id (str): The reservation id of a passenger trip
             first_name (str): The first name of the passenger
             last_name (str): The last name of the passenger
@@ -29,6 +29,33 @@ class Reservation:
 
     def __hash__(self) -> int:
         return hash(self.res_id)
+    
+class Timetable:
+    def __init__(self, is_departure: bool, city: str, station_name: str,
+                 start_time: str, arrival_time: str, num_stops: int):
+        """
+        Initialize a Timetable object.
+
+        Parameters:
+            is_departure (bool): 1 if train is departing, 0 if train is arriving
+            city (str): The city the train is arriving at/departing from
+            station_name (str): The station the train is arriving at/departing from
+            start_time (str): The start time of the trip
+            arrival_time (str): The arrival time of the trip
+            num_stops (int): The number of stops for the trip
+        """
+        self.is_departure = is_departure
+        self.city = city
+        self.station_name = station_name
+        self.start_time = start_time
+        self.arrival_time = arrival_time
+        self.num_stops = num_stops
+
+    def __repr__(self) -> str:
+        return f"Timetable({self.is_departure} {self.city})"
+    
+    #def display(self):
+
 
 class DBManager:
     def __init__(self, host: str, database: str, user: str, password: str):
@@ -58,7 +85,6 @@ class DBManager:
             )
         except Error as e:
             print('Error while attempting to connect to database', e)
-
 
     def disconnect(self):
         """ Disconnects from the PostgreSQL database. """
--- intermodal-db-project/src/main.py
+++ intermodal-db-project/src/main.py
@@ -1,8 +1,70 @@
 import os
 
-from db import Reservation, DBManager
+from db import Reservation, Timetable, DBManager
+from rich.console import Console
+from rich.table import Table
 
 db_name = 'intermodal train db'
+
+def display_reservations(reservations: list[Reservation], title: str):
+        """
+        Display a list of all reservation from a selected query.
+
+        Args:
+            title: The title of the table
+        """
+        console = Console()
+
+        table = Table(show_header=True, header_style="bold green", title=title)
+
+        table.add_column('Reservation ID')
+        table.add_column('First Name')
+        table.add_column('Last Name')
+        table.add_column('Start City')
+        table.add_column('Desination City')
+        table.add_column('Departure Time')
+        table.add_column('Duration')
+    
+        for res in reservations:
+            table.add_row(
+                str(res.res_id),
+                str(res.first_name),
+                str(res.last_name),
+                str(res.start),
+                str(res.dest),
+                str(res.start_time),
+                str(res.duration)
+            )
+
+        console.print(table)
+
+def display_timetable(timetable: list[Timetable], title: str):
+        """
+        Display a list of all timetable entries from a selected query.
+
+        Args:
+            title: The title of the table
+        """
+        console = Console()
+
+        table = Table(show_header=True, header_style="bold green", title=title)
+
+        table.add_column('City')
+        table.add_column('Station Name')
+        table.add_column('Start Time')
+        table.add_column('Arrival Time')
+        table.add_column('Number of Stops')
+    
+        for entry in timetable:
+            table.add_row(
+                str(entry.city),
+                str(entry.station_name),
+                str(entry.start_time),
+                str(entry.arrival_time),
+                str(entry.num_stops)
+            )
+
+        console.print(table)
 
 def main():
     user = os.getenv('DB_USER')
